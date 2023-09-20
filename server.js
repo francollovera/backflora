f4bd8d65f5bf235fc1b3ffe161197110103b4fa3
francollovera <francollovera@gmail.com>
2023-09-20 19:30:34 -0300

    ave

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const cors = require("cors");
+
 const path = require("path");
 
 const mercadopago = require("mercadopago");
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "./")));
-app.use(cors());
+
 
 app.get("/", function (req, res) {
   const filePath = path.resolve(__dirname, "carrito.html");
